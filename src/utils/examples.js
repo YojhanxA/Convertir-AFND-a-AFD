f49d519719bceac0a0e10f484235fb09e17b6bd3
Yojhan Arias <zigzagflete@gmail.com>
2026-05-24 17:16:41 -0500

    Clase

--- src/utils/examples.js
+++ src/utils/examples.js
@@ -1,14 +1,14 @@
 export const ejemplos = [
   {
-    nombre: "Ejemplo 1 - epsilon y no determinismo",
-    estados: "q0 q1 q2",
-    alfabeto: "a b",
-    estadoInicial: "q0",
-    estadosFinales: "q2",
-    transiciones: `q0,a,q0|q1
-q0,b,q0
-q1,b,q2
-q1,ε,q2`,
+    nombre: "Ejemplo clase",
+    estados: "A B C",
+    alfabeto: "0 1",
+    estadoInicial: "A",
+    estadosFinales: "C",
+    transiciones: `A,0,A|B
+A,1,B|C
+B,0,B|C
+C,0,C`,
   },
   {
     nombre: "Ejemplo 2 - simple no determinístico",
